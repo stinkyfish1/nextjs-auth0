99c395c4c7e22f73ac873b27583a7166ce524532
stinkyfish1 <156199864+stinkyfish1@users.noreply.github.com>
2025-02-13 22:03:53 +0100

    fix: fixed profile auth and profile page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+"use client";
+
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
+import { useUser } from "@auth0/nextjs-auth0/client";
 
 const Home: React.FC = () => {
+  const { user, isLoading } = useUser();
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    if (!isLoading && !user) {
+      setMessage("You're not logged in!");
+    } else {
+      setMessage("");
+    }
+  }, [user, isLoading]);
+
+  const handleProfileClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    if (!user) {
+      e.preventDefault();
+      alert("You're not logged in!");
+    }
+  };
+
   return (
     <div className="content-layout px-44 py-8">
       <div className="text-center">
@@ -11,7 +32,7 @@ const Home: React.FC = () => {
           Go to Protected Page
         </Link>
         <p className="text-lg mt-4">Or</p>
-        <Link href="/profile" className="text-blue-500 mt-4">
+        <Link href="/profile" className="text-blue-500 mt-4" onClick={handleProfileClick}>
           Go to Profile Page
         </Link>
         <img src="/full_stack_web_developer_salary-1.jpg" alt="man" />
