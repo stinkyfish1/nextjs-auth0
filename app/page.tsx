9ee908b3f0baa6c4ec4cc768b7c96e183e2840cb
stinkyfish1 <156199864+stinkyfish1@users.noreply.github.com>
2025-02-13 11:00:59 +0100

    added: nextjs with auth0 setup'

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import { getSession } from "@auth0/nextjs-auth0";
+
+export default async function Home() {
+  const session = await getSession();
+  const user = session?.user;
+
+  console.log(user);
+  return (
+    <div className="flex justify-center items-center">
+      <p className="text-3xl">Home</p>
+    </div>
+  );
+}
---
+++ components/navbar.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useUser } from "@auth0/nextjs-auth0/client";
+import { SignupButton } from "@/components/signup-button";
+import { LoginButton } from "@/components/login-button";
+import { LogoutButton } from "@/components/logout-button";
+import React from "react";
+
+const NavBar = () => {
+  const { user, error, isLoading } = useUser();
+  return (
+    <div className="py-4 flex w-full justify-between bg-gray-800 px-44">
+      <div className="flex gap-8">
+        <a href="/">Home</a>
+        
+      </div>
+      <div className="flex gap-4">
+        {!user && !isLoading && (
+          <>
+            <SignupButton />
+            <LoginButton />
+          </>
+        )}
+        {user && !isLoading && (
+          <>
+            <LogoutButton />
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
