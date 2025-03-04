7692ee59ed58b39216a6fea545c4fc0a07a33c63
stinkyfish1 <156199864+stinkyfish1@users.noreply.github.com>
2025-03-04 11:17:28 +0100

    add: added styling

--- app/page.tsx
+++ app/page.tsx
@@ -26,16 +26,13 @@ const Home: React.FC = () => {
   return (
     <div className="content-layout px-44 py-8">
       <div className="text-center">
-        <h1 className="text-4xl font-bold mt-4">Welcome to the Home Page</h1>
-        <p className="text-lg mt-4">This is a public page accessible to everyone.</p>
-        <Link href="/auth-protected" className="text-blue-500 mt-4">
-          Go to Protected Page
+        <h1 className="text-4xl font-bold mt-4">Velkommen til min</h1>
+        <h1 className="text-4xl font-bold mt-4">Auth0 og Supabase NextJs</h1>
+        <h1 className="text-4xl font-bold mt-4 mb-8">Nettside</h1>
+        <Link href="/auth-protected" className="text-blue-500 mb-6">
+          Din authSide
         </Link>
-        <p className="text-lg mt-4">Or</p>
-        <Link href="/profile" className="text-blue-500 mt-4" onClick={handleProfileClick}>
-          Go to Profile Page
-        </Link>
-        <img src="/full_stack_web_developer_salary-1.jpg" alt="man that codes and is happy" />
+        
       </div>
     </div>
   );
